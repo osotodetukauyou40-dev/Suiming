087c0d84b3d75a5b2fc9c58ef836d2eaad9d06c0
osotodetukauyou40-dev <osotodetukauyou40@gmail.com>
2026-09-17 21:19:15 +0900

    fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Tweet.destroy_all
 Tweet.create!(
  name: "ヒツジのいらない枕",
  image_url:"https://thumbnail.image.rakuten.co.jp/@0_mall/33taiyo/cabinet/shironuki/imgrc0110897767.jpg"
